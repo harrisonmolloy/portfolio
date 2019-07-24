a6d05a462c660bff73aadb914b7c7d672d76723e
Harrison Molloy <hello@harrisonmolloy.com>
2019-07-24 23:14:13 +1200

    Adding images and slide functions *

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
 window.onload = function() {
   console.log("Window loaded Up BB");
+
+  // slider func
+  let imgName = "Harrison-Knight-Molloy-Portfolio-Slide-"
+  let imgExt = ".jpg"
+  let imgIndex = 0
+
+  function nextSlide(n) {
+    if (n <= 66) {
+      n++
+      return imgName + n + imgExt;
+    } else {
+      n = 0
+      return imgName + n + imgExt;
+    }
+  }
+
+  function prevSlide(n) {
+    if (n >= 1) {
+      n--
+      return imgName + n + imgExt;
+    } else {
+      n = 66
+      return imgName + n + imgExt;
+    }
+  }
+
+  
 };
