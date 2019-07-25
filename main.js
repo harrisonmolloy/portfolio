adc99184a215c1fca05bc977406f036d104109d6
Harrison Molloy <hello@harrisonmolloy.com>
2019-07-25 21:11:52 +1200

    Debug

--- main.js
+++ main.js
@@ -1,39 +1,42 @@
-window.addEventListener('load', function () {
-  console.log("Window has loaded");
+window.addEventListener(
+  "load",
+  function() {
+    console.log("Window has loaded");
 
-  var imgName = "/images/Harrison-Knight-Molloy-Portfolio-Slide-";
-  var imgExt = ".jpg";
-  var imgIndex = 1;
-  let slider = document.getElementById("slider-js")
+    var imgName = "/images/Harrison-Knight-Molloy-Portfolio-Slide-";
+    var imgExt = ".jpg";
+    var imgIndex = 1;
+    let slider = document.getElementById("slider-js");
 
-  function nextSlide() {
-    console.log("nextSlide() was fired");
-    if (imgIndex == 66) {
-      imgIndex = 1;
-      slider.src = imgName + imgIndex + imgExt;
-    } else {
-      imgIndex++
-      slider.src = imgName + imgIndex + imgExt;
+    function nextSlide() {
+      console.log("nextSlide() was fired");
+      if (imgIndex == 66) {
+        imgIndex = 1;
+        slider.src = imgName + imgIndex + imgExt;
+      } else {
+        imgIndex++;
+        slider.src = imgName + imgIndex + imgExt;
+      }
     }
-  }
 
-  function prevSlide() {
-    console.log("prevSlide() was fired");
-    if (imgIndex == 1) {
-      imgIndex = 66;
-      slider.src = imgName + imgIndex + imgExt;
-    } else {
-      imgIndex--
-      slider.src = imgName + imgIndex + imgExt;
+    function prevSlide() {
+      console.log("prevSlide() was fired");
+      if (imgIndex == 1) {
+        imgIndex = 66;
+        slider.src = imgName + imgIndex + imgExt;
+      } else {
+        imgIndex--;
+        slider.src = imgName + imgIndex + imgExt;
+      }
     }
-  }
 
-  document.addEventListener("click", function (e) {
-    if (e.x > window.outerWidth / 2 && e.y > 60) {
-      nextSlide();
-    } else if (e.y > 60){
-      prevSlide();
-    }
-  });
-
-}, false);
+    document.addEventListener("click", function(e) {
+      if (e.x > window.outerWidth / 2 && e.y > 60) {
+        nextSlide();
+      } else if (e.y > 60) {
+        prevSlide();
+      }
+    });
+  },
+  false
+);
